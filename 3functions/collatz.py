c50859a33957afb12f5399be30682c924193048b
Katy Solovewicz <katy@solovewicz.com>
2021-02-05 16:47:12 -0800

    Add try block to collatz.py

--- 3functions/collatz.py
+++ 3functions/collatz.py
@@ -1,11 +1,14 @@
 
 print("Tell us a number, dearie.")
-number = int(input())
 
-while number > 1:
-  if number % 2 == 0:
-    number = number/2
-    print(number)
-  else:
-    number = number * 3 + 1
-    print(number)
+try:
+  number = int(input())
+  while number > 1:
+    if number % 2 == 0:
+      number = number/2
+      print(number)
+    else:
+      number = number * 3 + 1
+      print(number)
+except:
+  print("Sorry, that is not a valid integer.")
